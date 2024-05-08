cc0295a63d66e7f16fbc769a8dd91f1040170cc7
LibzAna8 <anastasia.lib7@gmail.com>
2024-05-08 09:39:43 +0300

    commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "36zmjx",
   e2e: {
     baseUrl: "https://www.canadadealsonline.com",
     pokemonBaseAPIUrl: "https://pokeapi.co/api/v2",
